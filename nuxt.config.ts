d5401ccc9eba9d073393177fdeb94b5bb0c5ea8e
Aidil Saputra Kirsan <aidil@lecturer.itk.ac.id>
2025-10-20 08:05:01 +0000

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,7 +10,7 @@ export default defineNuxtConfig({
   
   app: {
     head: {
-      title: 'Portal Nilai Mahasiswa',
+      title: 'Grade Verse',
       meta: [
         { charset: 'utf-8' },
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
